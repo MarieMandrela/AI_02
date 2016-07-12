2dd667fbad76c6ae675ae70f78c6fbacc3840894
Marie <marie.h.mandrela@gmail.com>
2016-07-12 16:08:46 +0200

    Lower resolution influence map to find general area.

--- src/AI/DijkstraTokenAI.java
+++ src/AI/DijkstraTokenAI.java
@@ -9,7 +9,8 @@ public class DijkstraTokenAI extends TokenAI {
 	
 	private boolean[][][][] adjacency = new boolean[Constants.DIM][Constants.DIM][Constants.DIM][Constants.DIM];
 	private float[][] influence = new float[Constants.DIM][Constants.DIM];
-	private float gamma = 0.9f;
+	private float[][] half_infuence = new float[Constants.DIM / 2][Constants.DIM / 2];
+	private float gamma = 0.00001f;
 	private float[][] value = new float[Constants.DIM][Constants.DIM];
 	private boolean[][] reachable = new boolean[Constants.DIM][Constants.DIM];
 	
@@ -34,31 +35,8 @@ public class DijkstraTokenAI extends TokenAI {
 		this.rnd.setSeed(playerNum + tokenNum);
 		
 		initReachable();
-		initInfluence();
+		setInfluence();
 		setValues();
-	}
-	
-	private void initInfluence() {
-		for (int i = 0; i < Constants.DIM; i++) {
-			for (int j = 0; j < Constants.DIM; j++) {
-				float field = getBoard(i, j);
-				if (!this.reachable[i][j]) {
-					this.influence[i][j] = Float.MIN_VALUE;
-				} else if (field == this.playerNum) {
-					this.influence[i][j] = -1;
-				} else if (field >= 0 && field <= 3) {
-					this.influence[i][j] = 0;
-				} else {
-					this.influence[i][j] = 1;
-				}
-			}
-		}
-		
-		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < 3; j++) {
-				applyTokenInfluence(i, j);
-			}
-		}
 	}
 	
 	private void setInfluence() {
@@ -67,25 +45,46 @@ public class DijkstraTokenAI extends TokenAI {
 		for (int i = 0; i < Constants.DIM; i++) {
 			for (int j = 0; j < Constants.DIM; j++) {
 				float field = getBoard(i, j);
-				if (!this.reachable[i][j]) {
-					continue;
+				if (!this.reachable[i][j] || field == Constants.WALL) {
+					newVal = -10;
 				}
-				
-				if (field == this.playerNum) {
+				else if (field == this.playerNum) {
 					newVal = -1;
 				} else if (field >= 0 && field <= 3) {
-					newVal = 0;
-				} else {
+					newVal = 1;
+				} else if (field == Constants.EMPTY) {
 					newVal = 1;
 				}
 				
-				this.influence[i][j] = this.gamma * this.influence[i][j] + (1 - this.gamma) * newVal;
+				this.influence[i][j] = newVal;
 			}
 		}
 		
 		for (int i = 0; i < 4; i++) {
 			for (int j = 0; j < 3; j++) {
-				applyTokenInfluence(i, j);
+				if (this.tokenNum == j) {
+					continue;
+				}
+				
+				applyTokenInfluence((int)this.tokens[i][j][0], (int)this.tokens[i][j][1]);
+				
+				if (i == this.playerNum) {
+					applyTokenInfluence((int)this.tokens[i][j][0], (int)this.tokens[i][j][1]);
+				}
+			}
+		}
+		
+		for (int i = 0; i < Constants.DIM - 1; i++) {
+			for (int j = 0; j < Constants.DIM - 1; j++) {
+				this.half_infuence[i / 2][j / 2] = 0;
+			}
+		}
+		
+		for (int i = 0; i < Constants.DIM - 1; i++) {
+			for (int j = 0; j < Constants.DIM - 1; j++) {
+				if (this.influence[i][j] != -10) {
+					this.half_infuence[i / 2][j / 2] += this.influence[i][j];
+				}
 			}
 		}
 	}
@@ -99,11 +98,11 @@ public class DijkstraTokenAI extends TokenAI {
 			for (int j = -half; j <= half; j++) {
 				new_x = x + i;
 				new_y = y + j;
-				if (new_x >= 0 && new_x <= Constants.DIM &&
-					new_y >= 0 && new_y <= Constants.DIM &&
+				if (new_x >= 0 && new_x < Constants.DIM &&
+					new_y >= 0 && new_y < Constants.DIM &&
 					getBoard(new_x, new_y) != Constants.WALL) {
-					newVal = -(Constants.GAUSS[half + i][half + j] / Constants.GAUSSSIZE);
-					this.influence[new_x][new_y] = this.gamma * this.influence[new_x][new_y] + (1 - this.gamma) * newVal;
+					newVal = Constants.GAUSS[half + i][half + j];
+					this.influence[new_x][new_y] = this.influence[new_x][new_y] - newVal;
 				}
 			}
 		}
@@ -121,24 +120,35 @@ public class DijkstraTokenAI extends TokenAI {
 	public void run() {
 		int[] goal = new int[2];
 		float[] dir = new float[2];
+		int timeCounter = 5000;
+		int updateRate = 5000;
 		
 		while (true) {		
 			try {
-				getTarget(goal);
+				if (timeCounter >= updateRate ||
+					(this.tokens[this.playerNum][this.tokenNum][0] == goal[0] &&
+					 this.tokens[this.playerNum][this.tokenNum][1] == goal[1])) {
+					getTarget(goal);
+					timeCounter = 0;
+				}
+				
 				goSomewhere(limit(getX()), limit(getY()), goal[0], goal[1], dir);
 				directions[this.tokenNum][0] = dir[0];
 				directions[this.tokenNum][1] = dir[1];
             	if (this.tokenNum == 1) {
             		Thread.sleep(400);
+            		timeCounter += 400;
             	}
 				if (this.tokenNum == 2) {
             		Thread.sleep(800);
+            		timeCounter += 800;
             	}
             	if (this.tokenNum == 0) {
             		Thread.sleep(150);
 	            	directions[this.tokenNum][0] = 0;
 					directions[this.tokenNum][1] = 0;
 	            	Thread.sleep(100);
+	            	timeCounter += 250;
             	}
     		} catch (InterruptedException e) {
     			e.printStackTrace();
@@ -152,15 +162,27 @@ public class DijkstraTokenAI extends TokenAI {
 	}
 	
 	private void getMaxInfluence(int[] xy) {
-		float max = Float.MIN_VALUE;
+		float max = -10;
 		
-		for (int i = 0; i < Constants.DIM; i++) {
-			for (int j = 0; j < Constants.DIM; j++) {
-				if (this.influence[i][j] > max ||
-					(this.influence[i][j] == max && rnd.nextBoolean())) {
-					max = this.influence[i][j];
-					xy[0] = i;
-					xy[1] = j;
+		for (int i = 0; i < Constants.DIM / 2 - 1; i++) {
+			for (int j = 0; j < Constants.DIM / 2 - 1; j++) {
+				if (this.half_infuence[i][j] > max ||
+					(this.half_infuence[i][j] == max && rnd.nextBoolean())) {
+					max = this.half_infuence[i][j];
+					xy[0] = i * 2;
+					xy[1] = j * 2;
+				}
+			}
+		}
+		
+		max = -10;
+		
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 2; j++) {
+				if (this.influence[xy[0] + i][xy[1] + j] > max) {
+					max = this.influence[xy[0] + i][xy[1] + j];
+					xy[0] += i;
+					xy[1] += j;
 				}
 			}
 		}
@@ -299,10 +321,6 @@ public class DijkstraTokenAI extends TokenAI {
 		xy[1] = j;
 	}
 	
-	private boolean movePossible(int x, int y, int dirX, int dirY) {
-		return adjacency[x][y][limit(getX() + dirX)][limit(getY() + dirY)];
-	}
-	
 	private int getBoard(float x, float y) {
 		return this.board[limit(x)][limit(y)];
 	}
@@ -342,31 +360,6 @@ public class DijkstraTokenAI extends TokenAI {
 	}
 	
 	/**
-	 * Evaluate how interesting to go to a field is.
-	 * => Influence map
-	 */
-	private float influenceField(float x, float y) {
-		int field = getBoard(x, y);
-		float val = 0;
-		
-		if (field == Constants.WALL) {
-			return Float.MIN_VALUE;
-		}
-		
-		if (field >= 0 && field <= 3 && field != this.playerNum) {
-			val = 1;
-		}
-		if (field == this.playerNum) {
-			val = 6;
-		}
-		if (fieldOccupied(x, y)) {
-			val += 0.5;
-		}
-		
-		return val;
-	}
-	
-	/**
 	 * Evaluate how good its is to step on the given field.
 	 * Prefer painting over enemy color > empty > my color > wall
 	 * Try to avoid running into other tokens
@@ -382,7 +375,7 @@ public class DijkstraTokenAI extends TokenAI {
 			val = 6;
 		}
 		if (fieldOccupied(x, y)) {
-			val += 0.5;
+			val += 1;
 		}
 		
 		return val;
